81489956e2addd16737d98d0dc54eb4baca6bc19
tahedi-soyad <tahedisoyad054@gmail.com>
2023-08-31 21:50:58 +0600

    all done

--- scripts/phone.js
+++ scripts/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText, isShowAll) => {
+const loadPhone = async (searchText = "13", isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
@@ -22,13 +22,13 @@ const displayPhones = (phones, isShowAll) => {
     showButtonContainer.classList.add("hidden");
   }
 
-  console.log("is show all? ", isShowAll);
+  // console.log("is show all? ", isShowAll);
   // display only first 9 phones if not show all
   if (!isShowAll) {
     phones = phones.slice(0, 9);
   }
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     const phoneCard = document.createElement("div");
     phoneCard.classList = `card bg-gray-100 p-4 shadow-xl`;
     phoneCard.innerHTML = `<figure>
@@ -40,8 +40,8 @@ const displayPhones = (phones, isShowAll) => {
   <div class="card-body">
     <h2 class="card-title">${phone.phone_name}</h2>
     <p>${phone.slug}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <div class="card-actions justify-center">
+      <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
     </div>
   </div>`;
 
@@ -50,6 +50,37 @@ const displayPhones = (phones, isShowAll) => {
 
   // hide loader
   toggleLoading(false);
+};
+
+// handle show details
+const handleShowDetails = async (id) => {
+  console.log(id);
+  // load single data
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await res.json();
+  // console.log(data);
+  const phone = data.data;
+  showPhoneDetails(phone);
+};
+
+const showPhoneDetails = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById("phone-name");
+  phoneName.innerText = phone.name;
+
+  const showDetailContainer = document.getElementById("show-detail-container");
+  showDetailContainer.innerHTML = `
+    <img src="${phone.image}" alt="" />
+  <p><span>Storage: ${phone?.mainFeatures?.storage}</span></p> 
+  <p><span>Chipset: ${phone?.mainFeatures?.chipSet}</span></p>
+  <p><span>Releasedate: ${
+    phone?.releaseDate || "no release date available"
+  }</span></p>
+  <p><span>Displaysize: ${phone?.mainFeatures?.displaySize}</span></p>
+  `;
+  showDetails.showModal();
 };
 
 const handleSearch = (isShowAll) => {
@@ -74,3 +105,5 @@ const toggleLoading = (isLoading) => {
 const handleShowAll = () => {
   handleSearch(true);
 };
+
+loadPhone();
